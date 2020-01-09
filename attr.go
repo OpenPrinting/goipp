083cbf446fb4ab37f2960b99d358c3cdf31ea3e7
Alexander Pevzner <pzz@apevzner.com>
2020-01-10 00:08:04 +0300

    Refactoring

--- attr.go
+++ attr.go
@@ -12,69 +12,6 @@ type Attribute struct {
 	Values Values
 }
 
-// Type Values represents an array of Attribute values
-type Values []struct {
-	T Tag   // The tag
-	V Value // The value
-}
-
-// Add value to Values
-func (values *Values) Add(t Tag, v Value) {
-	*values = append(*values, struct {
-		T Tag
-		V Value
-	}{t, v})
-}
-
-// Type Value represents a attribute value
-type Value interface {
-	isValue()
-}
-
-// Type Integer represents an Integer value
-type Integer uint32
-
-func (Integer) isValue() {}
-
-// Type Boolean represents a Boolean value
-type Boolean bool
-
-func (Boolean) isValue() {}
-
-// Type Strings represents a string value
-type String string
-
-func (String) isValue() {}
-
-// Type Time represents a DateTime value
-type Time struct{ time.Time }
-
-func (Time) isValue() {}
-
-// Type Resolution represents a resolution value
-type Resolution struct {
-	Xres, Yres int   // X/Y resolutions
-	Units      uint8 // Resolution units
-}
-
-func (Resolution) isValue() {}
-
-// Type Range represents a range of integers
-type Range struct {
-	Lower, Upper int // Lower/upper bounds
-}
-
-func (Range) isValue() {}
-
-// Type StringWithLang represents a combination of
-// Two strings: one is a name of natural language and
-// second is a text on this language
-type StringWithLang struct {
-	Lang, Text string // Language and text
-}
-
-func (StringWithLang) isValue() {}
-
 // Add value to attribute's values
 func (a *Attribute) AddValue(tag Tag, val Value) {
 	a.Values.Add(tag, val)
