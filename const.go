62e946537b01b3d6926003404bef7e99aa1273d9
Alexander Pevzner <pzz@apevzner.com>
2020-01-10 19:19:29 +0300

    Added various constants

---
+++ const.go
@@ -0,0 +1,17 @@
+/* Go IPP - IPP core protocol implementation in pure Go
+ *
+ * Copyright (C) 2020 and up by Alexander Pevzner (pzz@apevzner.com)
+ * See LICENSE for license terms and conditions
+ *
+ * Various constants
+ */
+
+package main
+
+const (
+	// Default IPP version
+	DefaultVersion Version = 0x0200
+
+	// Content type for IPP messages
+	ContentType = "application/ipp"
+)
