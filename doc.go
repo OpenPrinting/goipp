5d605167e5fd0a6697361a022d0910a3e00df4f5
Alexander Pevzner <pzz@apevzner.com>
2020-01-12 20:32:39 +0300

    doc.go updated

--- doc.go
+++ doc.go
@@ -12,9 +12,9 @@ It doesn't implement high-level operations, such as "print a document",
 "cancel print job" and so on. It's scope is limited to proper generation
 and parsing of IPP requests and responses.
 
-	IPP protocol uses the following simple model:
-	1. Send a request
-	2. Receive a response
+    IPP protocol uses the following simple model:
+    1. Send a request
+    2. Receive a response
 
 Request and response both has a similar format, represented here
 by type Message, with the only difference, that Code field of
